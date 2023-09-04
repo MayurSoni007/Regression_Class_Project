b01f76b3d78fc96cee1b0e4c83f6bda15c9fdd9c
MayurSoni007 <mayurssoni007@gmail.com>
2023-09-04 19:01:09 +0530

    EDA commit

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 ## End To End ML Project
 
-
-### created an enviroment.
+### created a environment
 ```
 conda create -p venv python==3.8
 
 conda activate venv/
 ```
-
 ### Install all necessary libraries
 ```
-pip install -r requiremnts.txt
+pip install -r requirements.txt
 ```
--- setup.py
+++ setup.py
@@ -3,16 +3,13 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-
-# creating a function to open and read the "requirements.txt" file and execute the list of packages named in it (like numpy, pandas etc.).
-
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:                     # To avoid installing << -e . >>
+        if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
@@ -21,11 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='RegressorProject',
     version='0.0.1',
-    author='Mayur',
-    author_email='mayurssoni007@gmail.com',
-    install_requires=["requirements.txt"],
+    author='Krissh',
+    author_email='krish.naik@ineuron.ai',
+    install_requires=['requirements.txt'],
     packages=find_packages()
 )
-
-
-
